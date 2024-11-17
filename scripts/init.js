50ead2d88755e834a034f58ab440c595d876f2ca
David Jocher <davidjocher@gmx.de>
2024-11-17 23:39:50 +0100

    Next Try in the dark

--- scripts/init.js
+++ scripts/init.js
@@ -23,80 +23,36 @@ import { MeisterpersonParser } from './utils/meisterpersonParser.js';
 Hooks.once('init', async function() {
     console.log('DSA-Macros | Initializing DSA Macros Module');
 
-    // Register module to the global scope
-    game.modules.get('dsa-macros').api = {
-        macros: {
-            DSAAttack,
-            DSADamage,
-            DSAParade,
-            DSATalent,
-            DSAZoneArmor,
-            DSAZoneWounds,
-            DSANPCAction
-        },
-        dialogs: {
-            AttackDialog,
-            DamageDialog,
-            NPCDialog,
-            ParadeDialog,
-            TalentDialog,
-            ZoneArmorDialog,
-            ZoneWoundsDialog
-        },
-        utils: {
-            NPCParser,
-            MeisterpersonParser
-        }
-    };
+    // Register module settings if needed
+    game.settings.register('dsa-macros', 'moduleSettings', {
+        name: 'DSA Macros Settings',
+        scope: 'world',
+        config: false,
+        default: {},
+        type: Object
+    });
 });
 
 Hooks.once('ready', async function() {
     console.log('DSA-Macros | Ready');
 });
 
-// Register macros with Foundry VTT
-Hooks.on('hotbarDrop', async (bar, data, slot) => {
-    if (data.type === 'Macro') return;
-
-    // Create macro based on item type
-    let command = '';
-    switch (data.type) {
-        case 'Attack':
-            command = `game.modules.get('dsa-macros').api.macros.DSAAttack.execute()`;
-            break;
-        case 'Damage':
-            command = `game.modules.get('dsa-macros').api.macros.DSADamage.execute()`;
-            break;
-        case 'Parade':
-            command = `game.modules.get('dsa-macros').api.macros.DSAParade.execute()`;
-            break;
-        case 'Talent':
-            command = `game.modules.get('dsa-macros').api.macros.DSATalent.execute()`;
-            break;
-        case 'ZoneArmor':
-            command = `game.modules.get('dsa-macros').api.macros.DSAZoneArmor.execute()`;
-            break;
-        case 'ZoneWounds':
-            command = `game.modules.get('dsa-macros').api.macros.DSAZoneWounds.execute()`;
-            break;
-        case 'NPCAction':
-            command = `game.modules.get('dsa-macros').api.macros.DSANPCAction.execute()`;
-            break;
-        default:
-            return;
-    }
-
-    // Create the macro
-    let macro = await Macro.create({
-        name: `DSA ${data.type}`,
-        type: 'script',
-        img: data.img || 'icons/svg/dice-target.svg',
-        command: command,
-        flags: { 'dsa-macros': { type: data.type } }
-    });
-
-    // Assign the macro to the hotbar slot
-    game.user.assignHotbarMacro(macro, slot);
-
-    return false;
-});
+// Make classes available globally
+window.DSAMacros = {
+    DSAAttack,
+    DSADamage,
+    DSAParade,
+    DSATalent,
+    DSAZoneArmor,
+    DSAZoneWounds,
+    DSANPCAction,
+    AttackDialog,
+    DamageDialog,
+    NPCDialog,
+    ParadeDialog,
+    TalentDialog,
+    ZoneArmorDialog,
+    ZoneWoundsDialog,
+    NPCParser,
+    MeisterpersonParser
+};
